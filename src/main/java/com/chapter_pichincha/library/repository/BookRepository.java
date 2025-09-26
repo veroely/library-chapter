361d276259ac82b598e4446e0c711bb9fab2ef03
Veronica Vicente <vvicente@pichincha.com>
2025-09-26 09:42:59 -0500

    add search methods for books by title and author

--- src/main/java/com/chapter_pichincha/library/repository/BookRepository.java
+++ src/main/java/com/chapter_pichincha/library/repository/BookRepository.java
@@ -2,8 +2,13 @@ package com.chapter_pichincha.library.repository;
 
 import com.chapter_pichincha.library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    // Búsqueda por título (case insensitive y coincidencia parcial)
     List<Book> findByTitleContainingIgnoreCase(String title);
+
+    // Búsqueda por autor (case insensitive y coincidencia parcial)
+    List<Book> findByAuthorContainingIgnoreCase(String author);
 }
